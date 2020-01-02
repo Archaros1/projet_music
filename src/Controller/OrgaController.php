86dd6f43809f9ee194944da992a4354017cdfca9
Archaros1 <arthurjung@hotmail.fr>
2020-01-02 15:51:57 +0100

    fonction createOrga + template + route

---
+++ src/Controller/OrgaController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+use App\Entity\Organisateur;
+use App\Repository\OrganisateurRepository;
+
+
+class OrgaController extends AbstractController
+{
+    private $orgaRepo;
+
+    public function index()
+    {
+        return $this->render('organisateur/orga_home.html.twig');
+    }
+}
