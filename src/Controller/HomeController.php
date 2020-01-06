822b94718e75f98b949e5b21614e7aa9b5a19c5a
jalil <abdel.jakwar@hotmail.fr>
2020-01-06 16:48:15 +0100

    templates

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -15,6 +15,17 @@ class HomeController extends AbstractController
         return $this->render('pages/home.html.twig');
     }
 
+    public function VitrineEvent()
+    {
+        return $this->render('pages/vitrine_event.html.twig');
+    }
+    
+    public function FormAnnonces()
+    {
+        return $this->render('pages/FormAnnonces.html.twig');
+    }
+    
+    
     
 
     
