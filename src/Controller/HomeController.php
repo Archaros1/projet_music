b02977e981f3ae3b52c7db334558711a29938d5c
webforce3 <Vous@exemple.com>
2020-01-08 16:37:39 +0100

    modification du slider

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -53,7 +53,7 @@ class HomeController extends AbstractController
         $events = $paginator->paginate(
             $donnees, // Requête contenant les données à paginer (ici nos articles)
             $request->query->getInt('page', 1), // Numéro de la page en cours, passé dans l'URL, 1 si aucune page
-            6 // Nombre de résultats par page
+            2 // Nombre de résultats par page
         );
 
         return $this->render('pages/agenda.html.twig', ["events" => $events]);
