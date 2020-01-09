69b4b75739dd5aa9f0fe52f9f76eee00f647ef00
florian1107 <florian67@neuf.fr>
2020-01-09 10:59:16 +0100

    modif update page home + début page agenda admin

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -7,7 +7,6 @@ use App\Entity\Offre;
 use App\Entity\Organisateur;
 use App\Entity\Annonce;
 use App\Form\Groupe;
-use App\Form\Musicien;
 use App\Form\Event;
 use App\Form\OrganisateurFormType;
 use App\Form\GroupeFormType;
@@ -16,11 +15,11 @@ use App\Repository\StyleRepository;
 use App\Repository\OffreRepository;
 use App\Repository\AnnonceRepository;
 use App\Repository\GroupeRepository;
-use App\Repository\MusicienRepository;
 use App\Repository\EventRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Security\Core\Security;
+use Knp\Component\Pager\PaginatorInterface;
 
 
 
@@ -30,17 +29,15 @@ class AdminController extends AbstractController
     private $organisateurRepo;
     private $annonceRepo;
     private $groupeRepo;
-    private $musicienRepo;
     private $eventRepo;
     private $offreRepo;
     private $styleRepo;
 
-    public function __construct(OrganisateurRepository $organisateurRepository, AnnonceRepository $annonceRepository, GroupeRepository $groupeRepository, MusicienRepository $musicienRepository, EventRepository $eventRepository, OffreRepository $offreRepository, StyleRepository $styleRepository)
+    public function __construct(OrganisateurRepository $organisateurRepository, AnnonceRepository $annonceRepository, GroupeRepository $groupeRepository, EventRepository $eventRepository, OffreRepository $offreRepository, StyleRepository $styleRepository)
     {
         $this->organisateurRepo = $organisateurRepository;
         $this->annonceRepo = $annonceRepository;
         $this->groupeRepo = $groupeRepository;
-        $this->musicienRepo = $musicienRepository;
         $this->eventRepo = $eventRepository;
         $this->offreRepo = $offreRepository;
         $this->styleRepo = $styleRepository;
@@ -51,21 +48,18 @@ class AdminController extends AbstractController
         $organisateur = $this->organisateurRepo->findAll();
         $annonce = $this->annonceRepo->findAll();
         $groupe = $this->groupeRepo->findAll();
-        $musicien = $this->musicienRepo->findAll();
         $event = $this->eventRepo->findAll();
         $offre = $this->offreRepo->findAll();
         $style = $this->styleRepo->findAll();
-         return $this->render("admin/pages/home.html.twig", [
+         return $this->render("admin/pages/admin_home.html.twig", [
             "organisateurs" => $organisateur, 
             "annonces" => $annonce, 
-            "groupes" => $groupe, 
-            "musiciens" => $musicien, 
+            "groupes" => $groupe,  
             "events" => $event,
             "offres" => $offre,
             "styles" =>$style
             ]);
     }
-
 
     public function updateOrganisateur(Request $request, Security $security, $id){
         $organisateur = $this->organisateurRepo->find($id);
@@ -76,7 +70,7 @@ class AdminController extends AbstractController
             $entityManager = $this->getDoctrine()->getManager();
             $entityManager->persist($organisateur);
             $entityManager->flush();
-            return $this->redirectToRoute("home");
+            return $this->redirectToRoute("admin_home");
         }
         return $this->render('admin/pages/update_orga.html.twig', [
             "organisateurForm" => $form->createView()]);
@@ -91,9 +85,23 @@ class AdminController extends AbstractController
             $entityManager = $this->getDoctrine()->getManager();
             $entityManager->persist($groupe);
             $entityManager->flush();
-            return $this->redirectToRoute("home");
+            return $this->redirectToRoute("admin_home");
         }
         return $this->render('admin/pages/update_groupe.html.twig', ["groupeForm" => $form->createView()]);
+    }
+
+    public function updateOffre(Request $request, Security $security, $id){
+        $offre = $this->offreRepo->find($id);
+        $form = $this->createForm(OffreFormType::class, $offre);
+        $form->handleRequest($request);
+        if($form->isSubmitted()){
+            $offre = $form->getData();
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($offre);
+            $entityManager->flush();
+            return $this->redirectToRoute("admin_home");
+        }
+        return $this->render('admin/pages/update_offre.html.twig', ["offreForm" => $form->createView()]);
     }
 
     public function updateAnnonce(Request $request, Security $security, $id){
@@ -105,7 +113,7 @@ class AdminController extends AbstractController
             $entityManager = $this->getDoctrine()->getManager();
             $entityManager->persist($annonce);
             $entityManager->flush();
-            return $this->redirectToRoute("home");
+            return $this->redirectToRoute("admin_home");
         }
         return $this->render('admin/pages/update_annonce.html.twig', ["annonceForm" => $form->createView()]);
     }
@@ -120,7 +128,7 @@ class AdminController extends AbstractController
             $entityManager = $this->getDoctrine()->getManager();
             $entityManager->persist($event);
             $entityManager->flush();
-            return $this->redirectToRoute("home");
+            return $this->redirectToRoute("admin_home");
         }
         return $this->render('admin/pages/update_event.html.twig', ["eventForm" => $form->createView()]);
     }
