9cc1d1c2767b5366b66d908e866210ebd4fa1064
Archaros1 <arthurjung@hotmail.fr>
2020-01-10 15:19:40 +0100

    blacklist opérationnel !

--- src/Controller/OrgaController.php
+++ src/Controller/OrgaController.php
@@ -9,6 +9,9 @@ use Symfony\Component\Security\Core\Security;
 use App\Entity\Organisateur;
 use App\Repository\OrganisateurRepository;
 
+use App\Entity\Groupe;
+use App\Repository\GroupeRepository;
+
 use App\Entity\Account;
 use App\Repository\AccountRepository;
 
@@ -18,19 +21,27 @@ use App\Repository\AnnonceRepository;
 use App\Entity\Event;
 use App\Repository\EventRepository;
 
+use App\Entity\Offre;
+use App\Repository\OffreRepository;
+
 
 class OrgaController extends AbstractController
 {
     private $orgaRepo;
+    private $groupeRepo;
     private $accountRepo;
     private $annonceRepo;
+    private $offreRepo;
+    private $eventRepo;
     private $user;
     private $security;
 
-    public function __construct(OrganisateurRepository $orgaRepository, AccountRepository $accountRepository, AnnonceRepository $annonceRepository, Security $security, EventRepository $eventRepository){
+    public function __construct(OffreRepository $offreRepository, GroupeRepository $groupeRepository, OrganisateurRepository $orgaRepository, AccountRepository $accountRepository, AnnonceRepository $annonceRepository, Security $security, EventRepository $eventRepository){
         $this->orgaRepo = $orgaRepository;
+        $this->groupeRepo = $groupeRepository;
         $this->annonceRepo = $annonceRepository;
         $this->accountRepo = $accountRepository;
+        $this->offreRepo = $offreRepository;
         $this->eventRepo = $eventRepository;
         $this->security = $security;
         $this->user = $security->getUser();
@@ -54,6 +65,31 @@ class OrgaController extends AbstractController
 
 
         return $this->render('organisateur/orga_home.html.twig', ["orga"=>$orga, "annonces"=>$annonces, "events"=>$events]);
+    }
+
+    public function blacklist($idGroupe)
+    {
+        $entityManager = $this->getDoctrine()->getManager();
+
+        $groupe = $this->groupeRepo->findOneById($idGroupe);
+        $orga = $this->user->getOrganisateur();
+        $orga->addBlacklist($groupe);
+
+        $entityManager->persist($orga);
+        $entityManager->persist($groupe);
+        $entityManager->flush();
+
+        $annonces = $orga->getAnnonces();
+
+        foreach ($annonces as $annonce) {
+            $offresEnCommun = $this->offreRepo->findAllInCommon($annonce->getId(), $idGroupe);
+            foreach ($offresEnCommun as $offre) {
+                $entityManager->remove($offre);
+                $entityManager->flush();
+            }
+        }
+
+        return $this->redirectToRoute("user_home");
     }
     
 }
